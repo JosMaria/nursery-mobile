0d49f41acbaed00df6e5fc39d94f8f2b7aabe0ed
Jose Maria <josmariaguilar@gmail.com>
2026-03-30 10:49:23 -0400

    Modify tab of catalog

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,26 +1,31 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
 
+import { Ionicons } from '@expo/vector-icons';
+
 import { HapticTab } from '@/components/haptic-tab';
 import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: 'white',
+        tabBarInactiveTintColor: '#9e9e9e',
+        tabBarStyle: {
+          backgroundColor: '#0D5302',
+          padding: 4,
+          height: 'auto'
+
+        },
         headerShown: false,
         tabBarButton: HapticTab,
       }}>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: 'Catalog',
+          tabBarIcon: ({ color }) => <Ionicons size={28} name="leaf" color={color} />,
         }}
       />
       <Tabs.Screen
