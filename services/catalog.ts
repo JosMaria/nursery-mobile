3a8b3c581d3a383210c6c8ce8ace992cad8fafc5
Jose Maria <josmariaguilar@gmail.com>
2026-03-30 11:32:27 -0400

    add and use axios

---
+++ services/catalog.ts
@@ -0,0 +1,16 @@
+import { api } from './api';
+
+export const catalogService = {
+    fetchPlantCards: async () => {
+        const { data } = await api.get<PlantCardResponse[]>('/plants/cards');
+        return data;
+    }
+};
+
+interface PlantCardResponse {
+    id: number;
+    scientific_name: string;
+    common_name: string;
+    price: number;
+    url: string;
+}
