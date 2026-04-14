1d2303da2a19723bb244580d38405137be799521
Jose Maria <josmariaguilar@gmail.com>
2026-04-13 20:09:31 -0400

    ajust main layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,5 @@
 import { Tabs } from 'expo-router';
-import { Platform } from 'react-native';
+import { Platform, View } from 'react-native';
 import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import { Ionicons } from '@expo/vector-icons';
@@ -11,50 +11,51 @@ export default function RootLayout() {
   return (
     <QueryClientProvider client={queryClient}>
       <SafeAreaProvider>
-        <TabNavigator />    
+        <MainLayout />
       </SafeAreaProvider>
     </QueryClientProvider>
   );
 }
 
-function TabNavigator() {
+const MainLayout = () => {
   const insets = useSafeAreaInsets();
-  const bottomPadding = Platform.OS === 'android' ? insets.bottom : 0;
-  console.log(bottomPadding);
- 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: '#fff',
-        tabBarInactiveTintColor: '#dad7cd',
-        tabBarStyle: {
-          backgroundColor: '#344e41',
-          height: 50 + bottomPadding
-        },
-        sceneStyle: {
-          backgroundColor: '#a3b18a'
-        }
-      }}
-    >
-      <Tabs.Screen
-        name='(plants)'
-        options={{
-          headerShown: false,
-          tabBarLabel: 'Plantas',
-          tabBarIcon: ({ focused, color, size }) => (
-            <Ionicons name={focused ? 'leaf-sharp' : 'leaf-outline'} color={color} size={size} />
-          )
-        }}
+    <View style={{ flex: 1 }}>
+      <View style={{
+          height: Platform.OS === 'android' ? insets.top : 0, 
+          backgroundColor: '#3a5a40'
+        }} 
       />
-      <Tabs.Screen
-        name='information'
-        options={{
-          tabBarLabel: 'Información',
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name='information-circle' color={color} size={size} />
-          )
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: 'white',
+          tabBarInactiveTintColor: '#dad7cd',
+          tabBarStyle: {
+            backgroundColor: '#3a5a40',
+            height: Platform.OS === 'android' ? insets.bottom + 40 : 'auto'
+          }
         }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name='(plants)'
+          options={{
+            headerShown: false,
+            tabBarLabel: 'Plantas',
+            tabBarIcon: ({ focused, color, size }) => (
+              <Ionicons name={focused ? 'leaf-sharp' : 'leaf-outline'} color={color} size={size} />
+            )
+          }}
+        />
+        <Tabs.Screen
+          name='information'
+          options={{
+            tabBarLabel: 'Información',
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name='information-circle' color={color} size={size} />
+            )
+          }}
+        />
+      </Tabs>
+    </View>
   );
 }
